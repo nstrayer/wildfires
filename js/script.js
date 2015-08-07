5ad6dad22bc04add775b9ad8eaee692322c95d3c
Nicholas Strayer <nstrayer@uvm.edu>
2015-08-06 23:27:34 -0700

    Centered the projection on the US

--- js/script.js
+++ js/script.js
@@ -30,7 +30,17 @@ var projection = d3.geo.orthographic()
     .scale(320)
     .translate([width / 2, height / 2])
     .clipAngle(90)
+    .rotate([90,-35,8])
     .precision(.1);
+
+// var projection = d3.geo.satellite()
+//     .distance(1.1)
+//     .scale(5500)
+//     .rotate([76.00, -35.50, 32.12])
+//     .center([-2, 5])
+//     .tilt(25)
+//     .clipAngle(Math.acos(1 / 1.1) * 180 / Math.PI - 1e-6)
+//     .precision(.1);
 
 var path = d3.geo.path()
     .projection(projection);
@@ -82,6 +92,8 @@ function ready(error, topology, fires){
         drawCanvas(fires);
 
       }));
+
+    // svg.call(zoom);
 }
 
 function drawCanvas(data) {
