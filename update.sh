2dd7294fe09a4350ec15472b8888f5375b701ac7
nstrayer <nstrayer@uvm.edu>
2016-02-21 17:51:44 +0000

    automatic data update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 # a simple script to update the data for visualization and push to the github repo
-cd /Users/Nick/dataProjects/d3Stuff/whereDaFiresAt/
+cd /home/pi/scriptsToRun/wildfires
 
 # first we download
 curl -o data/fires.csv https://firms.modaps.eosdis.nasa.gov/active_fire/text/USA_contiguous_and_Hawaii_48h.csv
